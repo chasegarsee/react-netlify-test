c63bef86a200cafa908506e980bea552c09473c7
chasegarsee <chasegarsee@gmail.com>
2019-12-17 12:59:28 -0600

    moved file structure

---
+++ build/precache-manifest.576eb7e114083aaceed04283ef751c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ba86037e16d7fb4cd00cdfd0542d24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1469c6c2b026ef3f5eea",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1469c6c2b026ef3f5eea",
+    "url": "/static/js/main.2d4a0cf7.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
